67351b34a8303a0f5b31aee7f1056c7210678902
Phil Reese <philr8@gmail.com>
2021-05-15 15:49:33 -0400

    test: fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Give Thanks App", () => {
+  test("renders increment button", () => {
+    act(() => {
+      render(<App />);
+    });
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
