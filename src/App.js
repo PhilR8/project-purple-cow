9302d7e886435a4cfe9b85493bb13d7a5bdb17cb
Phil Reese <philr8@gmail.com>
2021-05-14 22:46:57 -0400

    feat: get and update count on load and click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import countapi from "countapi-js";
+
+const COUNT_API_KEY = "1ccb732e-b55a-4404-ad3f-0f99c02fe44e";
 
 function App() {
+  const [count, updateCount] = useState(0);
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      const result = await countapi.get(COUNT_API_KEY);
+      updateCount(result?.value ?? 0);
+    };
+
+    fetchCount();
+  }, []);
+
+  const clickHandler = async () => {
+    const result = await countapi.hit(COUNT_API_KEY);
+    updateCount(result?.value ?? 0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="count">{count}</div>
+      <div className="incrementor">
+        <Button variant="outlined" size="large" onClick={clickHandler}>
+          +1
+        </Button>
+      </div>
     </div>
   );
 }
